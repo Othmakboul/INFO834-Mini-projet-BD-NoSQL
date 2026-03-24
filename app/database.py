8df385b42ba66b6975bea732e7f8d7442418deb3
zakaria261 <zakaria_rahou@yahoo.fr>
2026-03-24 14:43:33 +0100

    ajout mongo manager et maj db

--- app/database.py
+++ app/database.py
@@ -1,18 +1,16 @@
 import redis 
 from pymongo import MongoClient
 
-def redis_connection():
-    client = redis.Redis(
+def get_redis_client():
+    return redis.Redis(
         host='localhost',
         port=6379,
         db=0,
         decode_responses=True
     )
-    return client
 
-
-def get_mongo_connection():
-    uri = "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=tchat-rs"
-    client = MongoClient(uri)
-    db = client["tchat_app"]
-    return db
+def get_mongo_db():
+    # Liste complète des membres pour que le driver trouve le nouveau "chef"
+    uri = "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=tchat-rs"    # On ajoute un timeout pour ne pas attendre 30s en cas de problème
+    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    return client["tchat_app"]
